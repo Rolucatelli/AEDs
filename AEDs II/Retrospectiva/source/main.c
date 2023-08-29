ded2cb66ed7765c706c5bb30bcbbb632f1e0715f
Rodrigo Luis Gasparino Lucatelli <rodrigo.lucatelli@sou.unifal-mg.edu.br>
2023-08-29 10:10:00 -0300

    Teste

--- AEDs II/Retrospectiva/source/main.c
+++ AEDs II/Retrospectiva/source/main.c
@@ -6,8 +6,8 @@
 int main()
 {
     conta cliente[100];
-    cliente[0].dinheiro = 100.0;
-    cliente[1].dinheiro = 0.0;
+    cliente[0].dinheiro = 1000.0;
+    cliente[1].dinheiro = 100.0;
     pagamento(cliente, 0, 1, 50);
 
     printf("Saldo Pagador: %f, Saldo Recebidor: %f\n", cliente[0].dinheiro, cliente[1].dinheiro);
