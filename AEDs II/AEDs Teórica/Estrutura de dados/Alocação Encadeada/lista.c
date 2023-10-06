148b9b9010a2a9007fff0b58fac7a4ad24a8d3dc
Rolucatelli <rolucatelli@gmail.com>
2023-10-05 23:35:54 -0300

    Lista duplamente encadeada feita

---
+++ AEDs II/AEDs Teórica/Estrutura de dados/Alocação Encadeada/lista.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no_
+{
+    int chave;
+    int valor;
+    struct no_ *prox;
+} no;
+
+no *alocarNo()
+{
+    no *retorno = malloc(sizeof(no));
+    printf("Informe a chave: ");
+    scanf("%d", &retorno->chave);
+    printf("Informe o valor: ");
+    scanf("%d", &(*retorno).valor);
+    retorno->prox = NULL;
+    return retorno;
+}
+
+void ler_menu(int *resposta)
+{
+    printf("\n-----------------------\n");
+    printf("escolha uma das opcoes:\n");
+    printf("0 - sair\n");
+    printf("1 - inserir\n");
+    printf("2 - remover\n");
+    printf("3 - imprimir crescente\n");
+    printf("4 - imprimir decrescente\n");
+    scanf("%d", resposta);
+    printf("-----------------------\n\n");
+}
+
+void desalocar_lista(no *ptlista)
+{
+    no *proximo = ptlista->prox;
+    while (proximo != ptlista)
+    {
+        no *temp = proximo->prox;
+        free(proximo);
+        proximo = temp;
+    }
+    free(ptlista);
+}
+
+// Lista
+void imprimir(no *ptlista)
+{
+    if (ptlista->prox == NULL)
+    {
+        printf("<lista vazia!>");
+        return;
+    }
+
+    ptlista = ptlista->prox;
+    while (ptlista != NULL)
+    {
+        printf("%d:%d", ptlista->chave, ptlista->valor);
+        ptlista = ptlista->prox;
+        printf(" -> ");
+        if (ptlista == NULL)
+            printf("NULL");
+    }
+}
+
+void main(int argc, char **argv)
+{
+
+    int resposta = 1;
+    while (resposta != 0)
+    {
+        ler_menu(&resposta);
+
+        if (resposta == 0)
+        {
+            // sair
+            return;
+        }
+        else if (resposta == 1)
+        {
+            // inserir
+
+            no *novo_no = alocarNo();
+        }
+        else if (resposta == 2)
+        {
+            // remover
+            int x;
+            printf("\n\t Digite a chave: ");
+            scanf("%d", &x);
+        }
+        else if (resposta == 3)
+        {
+            // imprimir ordem crescente
+            // imprimir(ptlista);
+        }
+        else
+        {
+            printf("Opção inválida\n");
+        }
+    }
+}
--- AEDs II/AEDs Teórica/Estrutura de dados/Alocação Sequencial/lista.c
+++ AEDs II/AEDs Teórica/Estrutura de dados/Alocação Sequencial/lista.c
@@ -1,17 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int buscaBin(){
-    
-}
-
-
-
-int main()
+typedef struct no_
 {
-    
-    return 0;
+    int chave;
+    int valor;
+} no;
+
+int busca(no *L, int n, int x)
+{
+    L[n].chave = x;
+    int i = 0;
+    while (L[i].chave != x)
+    {
+        i++;
+    }
+    if (i == n)
+    {
+        return -1;
+    }
+    return i;
 }
 
 
+no *alocarNo()
+{
+    no *retorno = malloc(sizeof(no));
+    printf("Informe a chave: ");
+    scanf("%d", &retorno->chave);
+    printf("Informe o valor: ");
+    scanf("%d", &(*retorno).valor);
+    return retorno;
+}
+
+void ler_menu(int *resposta)
+{
+    printf("\n-----------------------\n");
+    printf("escolha uma das opcoes:\n");
+    printf("0 - sair\n");
+    printf("1 - inserir\n");
+    printf("2 - remover\n");
+    printf("3 - imprimir crescente\n");
+    printf("4 - imprimir decrescente\n");
+    scanf("%d", resposta);
+    printf("-----------------------\n\n");
+}
+
+void main(int argc, char **argv)
+{
+
+    int resposta = 1;
+    while (resposta != 0)
+    {
+        ler_menu(&resposta);
+
+        if (resposta == 0)
+        {
+            // sair
+            return;
+        }
+        else if (resposta == 1)
+        {
+            // inserir
+
+            no *novo_no = alocarNo();
+        }
+        else if (resposta == 2)
+        {
+            // remover
+            int x;
+            printf("\n\t Digite a chave: ");
+            scanf("%d", &x);
+        }
+        else if (resposta == 3)
+        {
+            // imprimir ordem crescente
+            // imprimir(ptlista);
+        }
+        else
+        {
+            printf("Opção inválida\n");
+        }
+    }
+}
