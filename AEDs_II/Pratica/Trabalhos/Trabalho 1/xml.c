fe0885e81a9c573700451c35f36dc24c63a358a0
Rolucatelli <rolucatelli@gmail.com>
2023-10-25 11:48:30 -0300

    Mudei as pastas, adicionei trabalho do iago

--- AEDs_II/Pratica/Trabalhos/Trabalho 1/xml.c
+++ AEDs_II/Pratica/Trabalhos/Trabalho 1/xml.c
@@ -17,42 +17,120 @@
 #include <stdlib.h>
 
 typedef char *string;
+typedef struct no_
+{
+    string info;
+    struct no *prox;
+} no;
+
+void inserir(no **topo, no *novoNo)
+{
+    novoNo->prox = *topo;
+    *topo = novoNo;
+}
+
+no *remover(no **topo)
+{
+    if (*topo != NULL)
+    {
+        no *retorno = *topo;
+        *topo = retorno->prox;
+        return retorno;
+    }
+    return NULL;
+}
 
 int tamString(string texto){
     int i = 0;
-    while (texto[i] != "\0")
+    while (texto[i] != '\0')
     {
         i++;
     }
-    return i-1;
+    return i;
 }
 
 string concatena(string string1, string string2){
-    string retorno;
-    int tam1 = tamString(string1), tam2 = tamString(string2), i = 0;
+    int tam1 = tamString(string1);
+    int tam2 = tamString(string2);
+    int tam3 = tam1 + tam2;
+    string string3 = malloc(tam3 * sizeof(char));
+    int i = 0;
     while (i < tam1)
     {
-        retorno[i] = string1[i];
+        string3[i] = string1[i];
         i++;
     }
     int j = 0;
-    while (i< tam1+tam2)
+    while (j < tam2)
     {
-        retorno[i] = string2[j];
+        string3[i] = string2[j];
+        i++;
         j++;
     }
-    retorno[i] = "\0";
-    return retorno;
+    return string3;
 }
 
-int main(int argc, char const *argv[])
+string removePrimeiroChar(string string1){
+    int tam1 = tamString(string1);
+    string string2 = malloc((tam1-1) * sizeof(char));
+    int i = 1;
+    while (i < tam1)
+    {
+        string2[i-1] = string1[i];
+        i++;
+    }
+    return string2;
+}
+
+void imprimir(no *topo)
 {
-    string s1 = "Olá, ", s2;
+    if (topo == NULL)
+    {
+        printf("pilha vazia");
+        return;
+    }
 
-    printf("\n Digite seu nome: ");
-    // scanf("%s", s2);
+    printf("topo --> \n");
+    while (topo != NULL)
+    {
+        printf("\t%d", topo->info);
+        topo = topo->prox;
+        printf("\n \t| \n");
+    }
+    printf("\tNULL\n");
+}
 
-    printf("%d", sizeof(s1));
+no *criarNo(string info)
+{
+    no *novoNo = malloc(sizeof(no));
+    novoNo->info = info;
+    novoNo->prox = NULL;
+    return novoNo;
+}
+
+int main()
+{
+    no *topo = NULL;
+    FILE *arquivo;
+    // string nomeArquivo;
+    // printf("Informe o nome do arquivo: ");
+    // scanf("%s", nomeArquivo);
+    // concatena("./", nomeArquivo);
+    arquivo = fopen("./entrada.xml", "r");
+    if (arquivo == NULL)
+    {
+        printf("Erro ao abrir o arquivo\n");
+        return 1;
+    }
+    
+    
+    
+
+
+
+
+    fclose(arquivo);
+
 
     return 0;
 }
